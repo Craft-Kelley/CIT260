7b43c647a566341547385fdb4ae56c608c4c9516
Summer Smith <smmr.smith128@gmail.com>
2014-10-07 13:46:57 -0600

    Adding logic to buildBoard function in GameBoard class

--- src/minesweeper/GameBoard.java
+++ src/minesweeper/GameBoard.java
@@ -2,7 +2,6 @@ package minesweeper;
 
 /**
  * This function will build the game board
- * @author Casey
  */
 
 import java.util.ArrayList;
@@ -12,7 +11,8 @@ public class GameBoard {
   int numCols;
   int numRows;
   int gameLevel; //the level the game is on
-  ArrayList <Square> boardSquares;
+  int numMines;
+  Square[][] grid;
   
   /*
   * Constructor with parameters
@@ -24,12 +24,44 @@ public class GameBoard {
   }
   
   /*
+  * Calculates the number of mines that should
+  * be placed on the board, based on the level
+  * of difficulty the user has selected.
+  */
+  void calcNumMines(){
+    //TODO: add logic to do calculation based on difficulty level
+    
+    //For now...
+    numMines = 10;
+  }
+  
+  /*
   * This function fills the list of squares with 
   * squares, some of which are assigned to be mines,
   * while others are not mines.
   */
   void buildBoard(){
+    calcNumMines();
+    int rand = 0;
+    grid = new Square[numRows][numCols];
     
+    rand = (int)(Math.random()*100);
+    System.out.println("First: " + rand);
+    rand = (int)(Math.random());
+    System.out.println("Second: " + rand);
+    //Place mines
+    for (int i = 0; i < numMines; i++){
+      rand = (int)(Math.random()*100);
+       
+      
+    }
+
+    //Fill in empty squares
+    for (int i = 0; i < numRows; i++){
+      for (int k = 0; k < numCols; k++){
+        
+      }
+    }
   }
     
   /**
