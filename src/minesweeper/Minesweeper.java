1bcb1d7a3542b0f976dca001951e76424b9d58e3
Summer Smith <smmr.smith128@gmail.com>
2014-10-07 22:36:07 -0600

    Re-added getName and displayHelp in Minesweeper class

--- src/minesweeper/Minesweeper.java
+++ src/minesweeper/Minesweeper.java
@@ -16,12 +16,19 @@ package minesweeper;
 public class Minesweeper {
     public String level;
     private int minesLeft;
+    String name = "John Doe";
+    String instructions = "Welcome to the game of Minesweeper. \n"
+            + "You will be playing individually. The objective of the game \n "
+            + "is to find the empty squares while avoiding the mines.\n "
+            + "Good Luck!";
     
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-      
+      Minesweeper game = new Minesweeper();
+      game.getName();
+      game.displayHelp();
     }
     
     /*
@@ -33,5 +40,13 @@ public class Minesweeper {
       //Logic:
       //Display main menu
       //Redirect based on menu selections
+    }
+    
+    public void getName(){
+      System.out.println(name);
+    }
+   
+    public void displayHelp(){
+      System.out.println(instructions);
     }
 }
