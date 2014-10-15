762da5ce5cdab08f27322b976ca301b39f0aa8a3
Summer Smith <smmr.smith128@gmail.com>
2014-10-15 16:56:05 -0600

    Removed Menu class, began editing MainMenuView

--- src/minesweeper/MainMenuView.java
+++ src/minesweeper/MainMenuView.java
@@ -5,10 +5,41 @@
  */
 package minesweeper;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author Jacky Northgrave
  */
 public class MainMenuView {
+  
+    String menuName;
+  String greeting= "This is the Main Menu Page.";
+  private ArrayList <String> menuItems = null;
+    /*
+  * Constructor with parameters
+  */
+  MainMenuView(String name, ArrayList <String> items){
+    this.menuName = name;
+    this.menuItems = items;
+  }
+      /**
+  * This function will display the menu information
+  */ 
+  public void display(){
+    //Output menu name
     
+    System.out.println("");
+        
+    //Output menu items
+  }
+  
+  /**
+  * This function will display individual
+  * strings (menu items) as options for the menu
+  */ 
+  public void displayList(){
+  
+  }
+
 }
