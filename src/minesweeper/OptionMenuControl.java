56ae1b1f710ad90afd568d7af24f46faf92457ec
KingTitan1 <cra13009@byui.edu>
2014-10-16 11:05:03 -0600

    Test-jon

---
+++ src/minesweeper/OptionMenuControl.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package minesweeper;
+
+/**
+ *
+ * @author Jonathan
+ */
+public class OptionMenuControl {
+    public void alterDifficulty(int difficulty){
+        //get difficulty change
+    }
+    public void alterSetup(int setup){
+        //get chsnge of settings
+    }
+    public void alterAudio(int audio){
+        //get change of audio lvl
+    }
+}
