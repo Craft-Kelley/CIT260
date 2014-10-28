54ca8db71f78924aace53e417f2e4c310429a843
Summer Smith <smmr.smith128@gmail.com>
2014-10-28 15:07:30 -0600

    Fixed high score

--- src/minesweeper/HighScore.java
+++ src/minesweeper/HighScore.java
@@ -13,8 +13,8 @@ import java.util.ArrayList;
  */
 public class HighScore {
   
-ArrayList <String> names;
-ArrayList <Integer> scores;
+ArrayList <String> names = new ArrayList();
+ArrayList <Integer> scores = new ArrayList();
   
   /*
   *
