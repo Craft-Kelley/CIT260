901a87f1c5cbadd7e1a8a7c49d3f416f080673bd
KingTitan1 <cra13009@byui.edu>
2014-10-30 23:40:28 -0600

    1st attempt at function before help.

---
+++ src/minesweeper/CalcScore.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package minesweeper;
+
+/**
+ *
+ * @author Jonathan
+ */
+public class CalcScore {
+   /* functionCalculate= (calculates a player's score at the end of the game.)*/
+}
