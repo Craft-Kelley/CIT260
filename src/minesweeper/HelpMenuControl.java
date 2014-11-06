70219345837c194f5e7557f61a0f557fc3c6667b
Northgrave-Jacky <alt09001@byui.edu>
2014-11-06 12:12:00 -0600

    Created wrapped text for game instructions

--- src/minesweeper/HelpMenuControl.java
+++ src/minesweeper/HelpMenuControl.java
@@ -13,7 +13,11 @@ public class HelpMenuControl {
     
     public void displayInstructions(){
         System.out.println(
-            "\tThe game is Minesweeper. It is a one player game. Mines are hidden on a grid of tiles. Tiles will either be blank, numbered or filled with a mine. If you click on a tile with a mine it will end the game. If you click on a tile with a number it will reveal how many mines are touching the tile. "
+            "\tThe game is Minesweeper. It is a one player game. Mines are "
+            + "hidden on a grid of tiles. Tiles will either be blank, numbered "
+            + "or filled with a mine. If you click on a tile with a mine it will "
+            + "end the game. If you click on a tile with a number it will reveal "
+            + "how many mines are touching the tile. "
         );
     }
 }
