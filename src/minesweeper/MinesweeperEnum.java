ed3c78c65d8d45ff0de91247e3e47546659a1dee
caseykames <ame14002@byui.edu>
2014-11-28 23:24:22 -0700

    Attempted an individual assignment in MinesweeperEnum.java - it is commented out so it does not influence the game at all.  Comment will be deleted after grading.

--- src/minesweeper/MinesweeperEnum.java
+++ src/minesweeper/MinesweeperEnum.java
@@ -31,3 +31,24 @@ classes somehow and get the program to run with the enum. It runs but
 I do not know what nor how to put the connection into the other classes. - Jonathan*/
 }
 
+/* Casey's attempt at an individual project:
+
+public MinesweeperEnum enterName () {
+String strName = null;
+Scanner inFile = Minesweeper.getInputFile();
+
+System.out.println("\tPlease enter the name of the player");
+do {
+strName = inFile.nextLine();
+strName = strName.trim().toUpperCase();
+if (strName.length() < 1);
+System.out.println("\n\tError: You must enter a name or enter a letter");
+return MinesweeperEnum.ERROR;
+}
+strName = strName.trim;
+} while (strName == null;
+this.setName(strName);
+return MinesweeperEnum.PLAYING;
+}
+
+*/
