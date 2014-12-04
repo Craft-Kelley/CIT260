199d0a27bde5d6d5ac937db722c549aa06fdcce1
Northgrave-Jacky <alt09001@byui.edu>
2014-12-04 11:11:07 -0600

    Renamed and refactored MenuExceptions class to "MenuExceptions2" Added new MenuExceptions class

--- src/minesweeper/GameBoard.java
+++ src/minesweeper/GameBoard.java
@@ -12,7 +12,7 @@ package minesweeper;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Random;
-import Exceptions.MenuExceptions2;
+import Exceptions.MenuExceptions;
 
 public class GameBoard implements Serializable{
   private int numCols;
@@ -59,7 +59,7 @@ public class GameBoard implements Serializable{
   * be placed on the board, based on the level
   * of difficulty the user has selected.
   */
-  private int calcNumMines() throws MenuExceptions2{
+  private int calcNumMines() throws MenuExceptions{
     //beginner
     if (gameLevel == 1){
       double calcNumMine = (double) (25.0 *(numCols * numRows))/100;
@@ -84,7 +84,7 @@ public class GameBoard implements Serializable{
       return roundedCalcNumMine;
     }
     else{
-      throw new MenuExceptions2("ERROR: Unable to calculate number of mines");
+      throw new MenuExceptions("ERROR: Unable to calculate number of mines");
     }
     
     }
@@ -136,7 +136,7 @@ public class GameBoard implements Serializable{
         }
       }
     }
-      } catch (MenuExceptions2 E) {
+      } catch (MenuExceptions E) {
           System.out.println(E.getMessage());
       }
       finally{
