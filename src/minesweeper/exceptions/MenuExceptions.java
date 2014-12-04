b20fde7c786a68ee451b30dc4a8a752e35120a6d
Northgrave-Jacky <alt09001@byui.edu>
2014-12-04 12:01:50 -0600

    Added class for exceptions package

---
+++ src/minesweeper/exceptions/MenuExceptions.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package minesweeper.exceptions;
+
+/**
+ *
+ * @author Jacky Northgrave
+ */
+public class MenuExceptions extends Exception{
+        public MenuExceptions() {
+ 
+}
+    public MenuExceptions(String message) {
+        super(message);
+    } 
+    public MenuExceptions(String message, Throwable cause) {
+        super(message, cause);
+    } 
+    public MenuExceptions(Throwable cause) {
+        super(cause);
+    } 
+}
