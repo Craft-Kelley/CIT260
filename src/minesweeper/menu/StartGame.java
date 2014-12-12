34b534c84559b2fe2d7f1ff51d86024c8bc67d7b
Jonathan <Jonathan@JWCs>
2014-12-11 19:05:41 -0700

    Place-holder created

---
+++ src/minesweeper/menu/StartGame.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package minesweeper.menu;
+
+import minesweeper.Player;
+
+/**
+ *
+ * @author Jonathan
+ */
+public class StartGame {
+    //The code for starting the game!
+}
