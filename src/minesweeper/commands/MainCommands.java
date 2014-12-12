47a4ab3f06c73dd09709e487af9a2a8d00ff762c
Jonathan <Jonathan@JWCs>
2014-12-11 23:50:16 -0700

    Place-holder created to fix error I got.

---
+++ src/minesweeper/commands/MainCommands.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package minesweeper.commands;
+
+/**
+ *
+ * @author Jonathan
+ */
+public class MainCommands {
+    public void create(){
+        //Not sure what I nned to do with this class. Instructions said make it
+        //so I did.
+    }
+}
