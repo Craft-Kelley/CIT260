f70858c4fd0be71af29a6435c6e06a2772adb18f
Craft-Kelley <cra12028@byui.edu>
2014-12-16 17:01:30 -0600

    updated gameover play again button to call options frame instead of mainframe

--- src/minesweeper/frames/GameOver.java
+++ src/minesweeper/frames/GameOver.java
@@ -117,8 +117,9 @@ public class GameOver extends javax.swing.JFrame {
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
      this.dispose();
-    MainFrame mainFrame = new MainFrame();
-    mainFrame.setVisible(true);
+        OptionFrame option = new OptionFrame();
+        option.setVisible(true);
+        this.dispose();
     }//GEN-LAST:event_jButton1ActionPerformed
 
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
