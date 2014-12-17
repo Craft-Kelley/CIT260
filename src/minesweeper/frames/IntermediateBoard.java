cd85f9025085710b638551a33fee2fa249c54b6e
Northgrave-Jacky <alt09001@byui.edu>
2014-12-16 20:18:13 -0600

    Added Jacky Northgrave as author

--- src/minesweeper/frames/IntermediateBoard.java
+++ src/minesweeper/frames/IntermediateBoard.java
@@ -4,10 +4,12 @@
  * and open the template in the editor.
  */
 package minesweeper.frames;
+import minesweeper.menu.MainMenu;
+import minesweeper.frames.GameLevel;
 
 /**
  *
- * @author mattnorthgrave
+ * @author Jacky Northgrave
  */
 public class IntermediateBoard extends javax.swing.JFrame {
 
@@ -57,6 +59,9 @@ public class IntermediateBoard extends javax.swing.JFrame {
         jE5Button = new javax.swing.JButton();
         jPanel2 = new javax.swing.JPanel();
         jTextField1 = new javax.swing.JTextField();
+        jGameLevelButton = new javax.swing.JButton();
+        jMainMenuButton = new javax.swing.JButton();
+        jQuitButton = new javax.swing.JButton();
 
         jCheckBox1.setText("jCheckBox1");
 
@@ -68,175 +73,175 @@ public class IntermediateBoard extends javax.swing.JFrame {
 
         jPanel1.setBackground(new java.awt.Color(68, 205, 182));
 
-        jA1Button.setFont(new java.awt.Font("Menlo", 0, 24)); // NOI18N
+        jA1Button.setFont(new java.awt.Font("Menlo", 0, 18)); // NOI18N
         jA1Button.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 jA1ButtonActionPerformed(evt);
             }
         });
 
-        jA2Button.setFont(new java.awt.Font("Menlo", 0, 24)); // NOI18N
+        jA2Button.setFont(new java.awt.Font("Menlo", 0, 18)); // NOI18N
         jA2Button.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 jA2ButtonActionPerformed(evt);
             }
         });
 
-        jA3Button.setFont(new java.awt.Font("Menlo", 0, 24)); // NOI18N
+        jA3Button.setFont(new java.awt.Font("Menlo", 0, 18)); // NOI18N
         jA3Button.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 jA3ButtonActionPerformed(evt);
             }
         });
 
-        jA4Button.setFont(new java.awt.Font("Menlo", 0, 24)); // NOI18N
+        jA4Button.setFont(new java.awt.Font("Menlo", 0, 18)); // NOI18N
         jA4Button.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 jA4ButtonActionPerformed(evt);
             }
         });
 
-        jA5Button.setFont(new java.awt.Font("Menlo", 0, 24)); // NOI18N
+        jA5Button.setFont(new java.awt.Font("Menlo", 0, 18)); // NOI18N
         jA5Button.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 jA5ButtonActionPerformed(evt);
             }
         });
 
-        jB1Button.setFont(new java.awt.Font("Menlo", 0, 24)); // NOI18N
+        jB1Button.setFont(new java.awt.Font("Menlo", 0, 18)); // NOI18N
         jB1Button.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 jB1ButtonActionPerformed(evt);
             }
         });
 
-        jB2Button.setFont(new java.awt.Font("Menlo", 0, 24)); // NOI18N
+        jB2Button.setFont(new java.awt.Font("Menlo", 0, 18)); // NOI18N
         jB2Button.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 jB2ButtonActionPerformed(evt);
             }
         });
 
-        jB3Button.setFont(new java.awt.Font("Menlo", 0, 24)); // NOI18N
+        jB3Button.setFont(new java.awt.Font("Menlo", 0, 18)); // NOI18N
         jB3Button.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 jB3ButtonActionPerformed(evt);
             }
         });
 
-        jB4Button.setFont(new java.awt.Font("Menlo", 0, 24)); // NOI18N
+        jB4Button.setFont(new java.awt.Font("Menlo", 0, 18)); // NOI18N
         jB4Button.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 jB4ButtonActionPerformed(evt);
             }
         });
 
-        jB5Button.setFont(new java.awt.Font("Menlo", 0, 24)); // NOI18N
+        jB5Button.setFont(new java.awt.Font("Menlo", 0, 18)); // NOI18N
         jB5Button.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 jB5ButtonActionPerformed(evt);
             }
         });
 
-        jC1Button.setFont(new java.awt.Font("Menlo", 0, 24)); // NOI18N
+        jC1Button.setFont(new java.awt.Font("Menlo", 0, 18)); // NOI18N
         jC1Button.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 jC1ButtonActionPerformed(evt);
             }
         });
 
-        jC2Button.setFont(new java.awt.Font("Menlo", 0, 24)); // NOI18N
+        jC2Button.setFont(new java.awt.Font("Menlo", 0, 18)); // NOI18N
         jC2Button.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 jC2ButtonActionPerformed(evt);
             }
         });
 
-        jC3Button.setFont(new java.awt.Font("Menlo", 0, 24)); // NOI18N
+        jC3Button.setFont(new java.awt.Font("Menlo", 0, 18)); // NOI18N
         jC3Button.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 jC3ButtonActionPerformed(evt);
             }
         });
 
-        jC4Button.setFont(new java.awt.Font("Menlo", 0, 24)); // NOI18N
+        jC4Button.setFont(new java.awt.Font("Menlo", 0, 18)); // NOI18N
         jC4Button.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 jC4ButtonActionPerformed(evt);
             }
         });
 
-        jC5Button.setFont(new java.awt.Font("Menlo", 0, 24)); // NOI18N
+        jC5Button.setFont(new java.awt.Font("Menlo", 0, 18)); // NOI18N
         jC5Button.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 jC5ButtonActionPerformed(evt);
             }
         });
 
-        jD1Button.setFont(new java.awt.Font("Menlo", 0, 24)); // NOI18N
+        jD1Button.setFont(new java.awt.Font("Menlo", 0, 18)); // NOI18N
         jD1Button.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 jD1ButtonActionPerformed(evt);
             }
         });
 
-        jD2Button.setFont(new java.awt.Font("Menlo", 0, 24)); // NOI18N
+        jD2Button.setFont(new java.awt.Font("Menlo", 0, 18)); // NOI18N
         jD2Button.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 jD2ButtonActionPerformed(evt);
             }
         });
 
-        jD3Button.setFont(new java.awt.Font("Menlo", 0, 24)); // NOI18N
+        jD3Button.setFont(new java.awt.Font("Menlo", 0, 18)); // NOI18N
         jD3Button.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 jD3ButtonActionPerformed(evt);
             }
         });
 
-        jD4Button.setFont(new java.awt.Font("Menlo", 0, 24)); // NOI18N
+        jD4Button.setFont(new java.awt.Font("Menlo", 0, 18)); // NOI18N
         jD4Button.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 jD4ButtonActionPerformed(evt);
             }
         });
 
-        jD5Button.setFont(new java.awt.Font("Menlo", 0, 24)); // NOI18N
+        jD5Button.setFont(new java.awt.Font("Menlo", 0, 18)); // NOI18N
         jD5Button.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 jD5ButtonActionPerformed(evt);
             }
         });
 
-        jE1Button.setFont(new java.awt.Font("Menlo", 0, 24)); // NOI18N
+        jE1Button.setFont(new java.awt.Font("Menlo", 0, 18)); // NOI18N
         jE1Button.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 jE1ButtonActionPerformed(evt);
             }
         });
 
-        jE2Button.setFont(new java.awt.Font("Menlo", 0, 24)); // NOI18N
+        jE2Button.setFont(new java.awt.Font("Menlo", 0, 18)); // NOI18N
         jE2Button.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 jE2ButtonActionPerformed(evt);
             }
         });
 
-        jE3Button.setFont(new java.awt.Font("Menlo", 0, 24)); // NOI18N
+        jE3Button.setFont(new java.awt.Font("Menlo", 0, 18)); // NOI18N
         jE3Button.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 jE3ButtonActionPerformed(evt);
             }
         });
 
-        jE4Button.setFont(new java.awt.Font("Menlo", 0, 24)); // NOI18N
+        jE4Button.setFont(new java.awt.Font("Menlo", 0, 18)); // NOI18N
         jE4Button.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 jE4ButtonActionPerformed(evt);
             }
         });
 
-        jE5Button.setFont(new java.awt.Font("Menlo", 0, 24)); // NOI18N
+        jE5Button.setFont(new java.awt.Font("Menlo", 0, 18)); // NOI18N
         jE5Button.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 jE5ButtonActionPerformed(evt);
@@ -248,24 +253,76 @@ public class IntermediateBoard extends javax.swing.JFrame {
         jTextField1.setBackground(new java.awt.Color(0, 0, 153));
         jTextField1.setFont(new java.awt.Font("Menlo", 1, 36)); // NOI18N
         jTextField1.setForeground(new java.awt.Color(255, 255, 255));
-        jTextField1.setText("Intermediate");
+        jTextField1.setText("Intermediate Level");
         jTextField1.setBorder(null);
+        jTextField1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jTextField1ActionPerformed(evt);
+            }
+        });
+
+        jGameLevelButton.setFont(new java.awt.Font("Menlo", 0, 13)); // NOI18N
+        jGameLevelButton.setForeground(new java.awt.Color(0, 0, 153));
+        jGameLevelButton.setText("Change Game Level");
+        jGameLevelButton.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jGameLevelButtonActionPerformed(evt);
+            }
+        });
+
+        jMainMenuButton.setFont(new java.awt.Font("Menlo", 0, 13)); // NOI18N
+        jMainMenuButton.setForeground(new java.awt.Color(0, 0, 153));
+        jMainMenuButton.setText("Main Menu");
+        jMainMenuButton.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jMainMenuButtonActionPerformed(evt);
+            }
+        });
+
+        jQuitButton.setFont(new java.awt.Font("Menlo", 0, 13)); // NOI18N
+        jQuitButton.setForeground(new java.awt.Color(0, 0, 153));
+        jQuitButton.setText("Quit");
+        jQuitButton.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jQuitButtonActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
         jPanel2.setLayout(jPanel2Layout);
         jPanel2Layout.setHorizontalGroup(
             jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(jPanel2Layout.createSequentialGroup()
-                .addGap(166, 166, 166)
-                .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel2Layout.createSequentialGroup()
+                .addGap(0, 0, Short.MAX_VALUE)
+                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel2Layout.createSequentialGroup()
+                        .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(96, 96, 96))
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel2Layout.createSequentialGroup()
+                        .addComponent(jGameLevelButton)
+                        .addGap(204, 204, 204))))
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel2Layout.createSequentialGroup()
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel2Layout.createSequentialGroup()
+                        .addComponent(jQuitButton)
+                        .addGap(258, 258, 258))
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel2Layout.createSequentialGroup()
+                        .addComponent(jMainMenuButton)
+                        .addGap(236, 236, 236))))
         );
         jPanel2Layout.setVerticalGroup(
             jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel2Layout.createSequentialGroup()
                 .addGap(21, 21, 21)
                 .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(135, Short.MAX_VALUE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(jGameLevelButton)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jMainMenuButton)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jQuitButton)
+                .addContainerGap(24, Short.MAX_VALUE))
         );
 
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
@@ -505,40 +562,27 @@ public class IntermediateBoard extends javax.swing.JFrame {
         jE5Button.setText("1");        // TODO add your handling code here:
     }//GEN-LAST:event_jE5ButtonActionPerformed
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String args[]) {
-        /* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-         */
-        try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(IntermediateBoard.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(IntermediateBoard.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(IntermediateBoard.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(IntermediateBoard.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        }
-        //</editor-fold>
+    private void jTextField1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jTextField1ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jTextField1ActionPerformed
 
-        /* Create and display the form */
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                new IntermediateBoard().setVisible(true);
-            }
-        });
-    }
+    private void jGameLevelButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jGameLevelButtonActionPerformed
+       GameLevel gameLevel= new GameLevel();
+       gameLevel.setVisible(true);
+       this.dispose();
+    }//GEN-LAST:event_jGameLevelButtonActionPerformed
+
+    private void jMainMenuButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMainMenuButtonActionPerformed
+         MainFrame main = new MainFrame();
+         main.setVisible(true);
+         this.dispose();
+    }//GEN-LAST:event_jMainMenuButtonActionPerformed
+
+    private void jQuitButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jQuitButtonActionPerformed
+         this.dispose();
+    }//GEN-LAST:event_jQuitButtonActionPerformed
+
+
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton jA1Button;
@@ -567,8 +611,11 @@ public class IntermediateBoard extends javax.swing.JFrame {
     private javax.swing.JButton jE3Button;
     private javax.swing.JButton jE4Button;
     private javax.swing.JButton jE5Button;
+    private javax.swing.JButton jGameLevelButton;
+    private javax.swing.JButton jMainMenuButton;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JPanel jPanel2;
+    private javax.swing.JButton jQuitButton;
     private javax.swing.JTextField jTextField1;
     private javax.swing.JTextField jTextField2;
     // End of variables declaration//GEN-END:variables
